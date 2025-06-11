f7a73f1d2344d4bb66c6b26dae6963abeea8c53a
Anton Valdez <valdezanton97@gmail.com>
2025-06-11 16:57:45 +0800

    Add files via upload
    Updated Quizzler App

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
-
+from ui import QuizInterface
 question_bank = []
-for x in question_data:
-    question = Question(x["question"],x["correct_answer"])
-    question_bank.append(question)
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
 
 quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
 
-while quiz.still_has_questions():
-    quiz.next_question()
-
-print("\n\nYou have completed the quiz!")
-print(f"Your final score is: {quiz.score}/{quiz.question_number}")
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- quiz_brain.py
+++ quiz_brain.py
@@ -1,23 +1,26 @@
+import html
+
 class QuizBrain:
-    def __init__(self, question_list):
-        self.question_list = question_list
+
+    def __init__(self, q_list):
         self.question_number = 0
         self.score = 0
+        self.question_list = q_list
+        self.current_question = None
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
 
     def next_question(self):
-        current_question = self.question_list[self.question_number]
+        self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        answer = input(f"\nQ.{self.question_number}: {current_question.text} (True/False)? ").lower()
-        self.check_answer(answer, current_question.answer)
+        q_text = html.unescape(self.current_question.text)
+        return f"Q.{self.question_number}: {q_text}"
 
-    def check_answer(self, answer, correct_answer):
-        if answer.lower() == correct_answer.lower():
-            print("You got it right!")
+    def check_answer(self, user_answer):
+        correct_answer = self.current_question.answer
+        if user_answer.lower() == correct_answer.lower():
             self.score += 1
+            return True
         else:
-            print("That is wrong!")
-        print(f"The correct answer is: {correct_answer}")
-        print(f"Your current score is: {self.score}/{self.question_number}")
+            return False
